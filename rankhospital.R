c069d66c21f2b0bb4c0c3706c3df179e0f86274d
Ravi Sankar Krothapalli <rskrothapalli@gmail.com>
2014-09-01 11:43:14 -0600

    Function to return the Hospital Name in the state  based on the provided rank

---
+++ rankhospital.R
@@ -0,0 +1,8 @@
+rankhospital <- function(state, outcome, num = "best") {
+  ## Read outcome data
+  outcomedata <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  
+  ## Check that state and outcome are valid
+  ## Return hospital name in that state with the given rank
+  ## 30-day death rate
+}
